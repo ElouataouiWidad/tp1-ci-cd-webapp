b22dcbdbc931e31bb27495b3913a3f037eba2c46
ElouataouiWidad <98271360+ElouataouiWidad@users.noreply.github.com>
2025-09-17 21:45:48 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.end('Hello from CI/CD');
+  res.end('Hello from CI/CD !!!');
 });
 
 const port = process.env.PORT || 3000;
