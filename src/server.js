86bfb15b2c1c951a0dfe10bd5c39a4061b290eae
ElouataouiWidad <98271360+ElouataouiWidad@users.noreply.github.com>
2025-09-17 17:00:22 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 const http = require('http');
 const server = http.createServer((req,res)=>{
-   res.end('Hello from CI/CD in Azure');
+   res.end('Hello from CI/CD in Azure !!');
 });
 server.listen(3000);
